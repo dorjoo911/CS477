90164634b810fe9f1325bb4053ae45ec8e795267
dorjoo911 <tomike@asia.com>
2022-08-08 15:18:02 -0500

    followers bug killed

--- Project-Twitter/client/js/home.js
+++ Project-Twitter/client/js/home.js
@@ -23,6 +23,13 @@ async function fetchBlogs() {
   const token = sessionStorage.getItem("accessToken");
   const userData = parseJwt(token);
 
+  const wholeUsers = await fetch(`http://localhost:3000/users/`);
+  const arrData = await wholeUsers.json();
+  console.log(`arrData`, arrData);
+  const count = arrData.filter((i) =>
+    i.following.includes(userData.username)
+  ).length;
+
   // TODO - username to id
   const response = await fetch(`${HOSTNAME}/users/${userData.username}`, {
     headers: {
@@ -61,9 +68,7 @@ async function fetchBlogs() {
     <!-- Stats -->
     <div class="d-flex">
       <div class="col-4 text-center">
-        <a href="#" class="h4 font-bolder mb-0" id="numOfPosts">${
-          user.following.length
-        }</a>
+        <a href="#" class="h4 font-bolder mb-0" id="numOfPosts">${user.following.length}</a>
         <span class="d-block text-sm">Posts</span>
       </div>
       <div class="col-4 text-center">
@@ -71,7 +76,7 @@ async function fetchBlogs() {
         <span class="d-block text-sm">Following</span>
       </div>
       <div class="col-4 text-center">
-        <a href="#" class="h4 font-bolder mb-0">${user.following.length + 3}</a>
+        <a href="#" class="h4 font-bolder mb-0 id="numOfFollowers"">${count}</a>
         <span class="d-block text-sm">Followers</span>
       </div>
     </div>`;
@@ -90,7 +95,7 @@ async function deleteById(id) {
     },
   });
   const result = await response.json();
-  console.log(result);
+  // console.log(result);
   if (result.error) {
     alert(result.message);
   } else {
@@ -119,78 +124,6 @@ function parseJwt(token) {
   return JSON.parse(jsonPayload);
 }
 
-// async function postTweet() {
-//   const token = sessionStorage.getItem("accessToken");
-//   const userData = parseJwt(token);
-
-//   const post = document.getElementById("tweetPost").value;
-
-//   const response = await fetch(`http://localhost:3000/tweetS/`, {
-//     method: "POST",
-//     body: JSON.stringify({
-//       username: userData.username,
-//       comment: post,
-//     }),
-//     headers: {
-//       "Content-type": "application/json",
-//       Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
-//     },
-//   });
-//   showTweets10(userData.username);
-// }
-// async function showTweets10(username) {
-//   const response = await fetch(`http://localhost:3000/tweetS/${username}`);
-//   const result = await response.json();
-//   console.log(result);
-
-//   let html = "";
-//   for (let each of result) {
-//     html += `   <div class="card mb-3">
-//       <div class="card-body">
-//         <div class="d-flex flex-start">
-//           <!--insert photo link -->
-//           <img
-//             class="rounded-circle shadow-1-strong me-3"
-//             src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(22).webp"
-//             alt="avatar"
-//             width="40"
-//             height="40"
-//           />
-//           <div class="w-100">
-//             <div
-//               class="d-flex justify-content-between align-items-center mb-3"
-//             >
-//               <!--insert user name  and post body-->
-//               <h6 class="text-primary fw-bold mb-0">
-//                 ${each.username} <br/>
-//                 <span class="text-dark ms-2"
-//                   > ${each.comment}</span
-//                 >
-//               </h6>
-//               <!--insert date published-->
-
-//               <p class="mb-0">${each.date}</p>
-//             </div>
-//             <div
-//               class="d-flex justify-content-between align-items-center"
-//             >
-//               <p class="small mb-0" style="color: #aaa">
-//                 <a href="#!" class="link-grey">Like</a> •
-//                 <a href="#!" class="link-grey">Follow</a> •
-//               </p>
-//               <div class="d-flex flex-row">
-//                 <i class="fas fa-star text-warning me-2"></i>
-//                 <i class="far fa-check-circle" style="color: #aaa"></i>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>`;
-//   }
-//   document.getElementById("mydiv").innerHTML = html;
-//   console.log("----in display");
-// }
 async function showTweets10(username) {
   // console.log(username);
   const resp = await fetch(`http://localhost:3000/users/`);
@@ -198,8 +131,8 @@ async function showTweets10(username) {
   // console.log(`*****`, resData[0].photo);
   const response = await fetch(`http://localhost:3000/tweetS/${username}`);
   const result = await response.json();
-  const arr = result.filter((data) => data.username === username);
 
+  const arr = result.filter((data) => data.username === username);
   document.getElementById("numOfPosts").innerHTML = arr.length;
 
   let html = "";
@@ -261,11 +194,11 @@ async function showTweets10(username) {
       </div>`;
   }
   document.getElementById("showTweet").innerHTML = html;
-  console.log("----in display");
+  // console.log("----in display");
 }
 
 async function postTweet() {
-  console.log("-----in postTweet");
+  // console.log("-----in postTweet");
   const token = sessionStorage.getItem("accessToken");
   const userData = parseJwt(token);
 
